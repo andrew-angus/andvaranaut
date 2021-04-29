f9bd949e7c4ad0d34af6b9f52fa49def4074f847
Andrew Angus <andrew.angus@warwick.ac.uk>
2021-04-29 13:24:56 +0100

    Working basic parallelism on target function evaluation

--- andvaranaut/forward.py
+++ andvaranaut/forward.py
@@ -7,6 +7,7 @@ import scipy.stats as st
 from time import time as stopwatch
 from seaborn import kdeplot
 import matplotlib.pyplot as plt
+from andvaranaut.utils import *
 
 # Latin hypercube sampler and propagator
 class lhc():
@@ -38,22 +39,25 @@ class lhc():
 
   # Private method which takes array of x samples and evaluates y at each
   # ToDo add parallel execution
-  def __vector_solver(self,xsamps):
+  def __vector_solver(self,xsamps,parallel,nproc):
     t0 = stopwatch()
-    n_samples = len(xsamps)
-    ysamps = np.zeros((n_samples,self.ny))
-    for i in range(n_samples):
-      try:
-        ysamps[i,:] = self.target(xsamps[i,:])
-      except:
-        self.x = np.r_[self.x,xsamps[:i,:]]
-        self.y = np.r_[self.y,ysamps[:i,:]]
-        errstr = f"Error: Target function evaluation failed at sample {i+1} "+\
-            "with xsamples: " +str(xsamps[i,:])+\
-            "\nCheck number of inputs/outputs is correct and range of input values valid."+\
-            f"\n{i} samples succesfully added to dataset."
-        raise Exception(errstr)
-      print(f'Run is {(i+1)/n_samples:0.1%} complete.',end='\r')
+    if parallel:
+      ysamps = parallel_runs(self.target,xsamps,nproc)
+    else:
+      n_samples = len(xsamps)
+      ysamps = np.zeros((n_samples,self.ny))
+      for i in range(n_samples):
+        try:
+          ysamps[i,:] = self.target(xsamps[i,:])
+        except:
+          self.x = np.r_[self.x,xsamps[:i,:]]
+          self.y = np.r_[self.y,ysamps[:i,:]]
+          errstr = f"Error: Target function evaluation failed at sample {i+1} "+\
+              "with xsamples: " +str(xsamps[i,:])+\
+              "\nCheck number of inputs/outputs is correct and range of input values valid."+\
+              f"\n{i} samples succesfully added to dataset."
+          raise Exception(errstr)
+        print(f'Run is {(i+1)/n_samples:0.1%} complete.',end='\r')
     print('Run is 100.0% complete.')
     t1 = stopwatch()
     print(f'Time taken: {t1-t0:0.1e} s')
@@ -63,13 +67,13 @@ class lhc():
     self.y = np.r_[self.y,ysamps]
 
   # Add n samples to current via latin hypercube sampling
-  def sample(self,nsamps):
+  def sample(self,nsamps,parallel=False,nproc=None):
     points = latin_random(nsamps,self.nx)
     xsamps = np.zeros((nsamps,self.nx))
     for i in range(nsamps):
       for j in range(self.nx):
         xsamps[i,j] = self.dists[j].ppf(points[i,j])
-    self.__vector_solver(xsamps)
+    self.__vector_solver(xsamps,parallel,nproc)
 
   # Delete n samples by random indexing
   # ToDo: Better to delete by low resolution latin hypercube?
@@ -112,6 +116,7 @@ class lhc():
     y_samps[nsamps:,:] = old_ysamps
     y_scaled = copy.deepcopy(y_samps)
     y_scaled = y_convert(y_scaled)
+ 
 
 # Inherit from LHC class and add data conversion methods
 class _surrogate(lhc):
--- andvaranaut/utils.py
+++ andvaranaut/utils.py
@@ -2,6 +2,8 @@
 
 import ray
 import multiprocessing as mp
+import os
+import numpy as np
 
 
 # Function which wraps serial function for executing in parallel directories
@@ -19,25 +21,24 @@ def __parallel_wrap(inp,idx,func):
 # Returning 2D array of outputs
 def parallel_runs(func,inps,nps):
     
-    # Ensure number of requested processors is reasonable
-    assert (nps <= mp.cpu_count()),\
-        "Error: number of processors selected exceeds available."
-    
-    # Create parallel directory for tasks
-    os.system('mkdir parallel')
+  # Ensure number of requested processors is reasonable
+  assert (nps <= mp.cpu_count()),\
+      "Error: number of processors selected exceeds available."
+  
+  # Create parallel directory for tasks
+  os.system('mkdir parallel')
 
-    # Run function in parallel    
-    ray.init(num_cpus=nps,log_to_driver=False)
-    l = len(inps)
-    outs = ray.get([__parallel_wrap.remote(inps[i],i,func) for i in range(l)])
-    ray.shutdown()
-    
-    # Reshape outputs to 2D array
-    if isinstance(outs[0],list):
-      ll = len(outs[0])
-    else:
-      ll = 1
-    outs = np.array(outs).reshape((l,ll))
+  # Run function in parallel    
+  ray.init(num_cpus=nps,log_to_driver=False)
+  l = len(inps)
+  outs = ray.get([__parallel_wrap.remote(inps[i],i,func) for i in range(l)])
+  ray.shutdown()
+  
+  # Reshape outputs to 2D array
+  if isinstance(outs[0],np.ndarray):
+    outs = np.array(outs)
+  else:
+    outs = np.array(outs).reshape((l,1))
 
-    return outs
+  return outs
 
