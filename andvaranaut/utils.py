a5bc8e39a949ce59964fb836142c0bd278d6ea8e
Andrew Angus <andrew.angus@warwick.ac.uk>
2021-05-02 14:04:06 +0100

    Reformat standard con/rev methods to work for individual variables

--- andvaranaut/utils.py
+++ andvaranaut/utils.py
@@ -25,32 +25,28 @@ def __logit(x):
   x = np.maximum(1.0-bnd,x)
   return lg(x)
 # Logit transform using uniform dists
-def logit(x,dists):
+def logit(x,dist):
   # Convert uniform distribution samples to standard uniform [0,1]
   # and logit transform to unbounded range 
-  x01 = std_uniform(x,dists)
+  x01 = std_uniform(x,dist)
   x = __logit(x01)
   return x
 # Convert uniform dist samples into standard uniform 
-def std_uniform(x,dists):
-  x01 = np.zeros_like(x)
-  for i in range(len(dists)):
-    intv = dists[i].interval(1.0)
-    x01[:,i] = (x[:,i]-intv[0])/(intv[1]-intv[0])
-  return x01
+def std_uniform(x,dist):
+  intv = dist.interval(1.0)
+  x = (x-intv[0])/(intv[1]-intv[0])
+  return x
 # Convert normal dist samples into standard normal
-def std_normal(x,dists):
-  xs = np.zeros_like(x)
-  for i in range(len(dists)):
-    xs[:,i] = (x[:,i]-dists[i].mean())/dists[i].std()
-  return xs
+def std_normal(x,dist):
+  x = (x-dist.mean())/dist.std()
+  return x
 # Convert positive values to unbounded with logarithm
 def log_con(y):
   return np.log10(y)
 # Convert non-negative to unbounded, via intermediate [0,1]
 def nonneg_con(y):
   y01 = y/(1+y)
-  return  __logit(y)
+  return  __logit(y01)
 
 ## Reversion functions
 
@@ -65,22 +61,18 @@ def __logistic(x):
   ex = np.exp(sign*x)
   return 0.50-sign*0.50+sign*ex/(ex+1.0)
 # Logistic transform using uniform dists
-def logistic(x,dists):
+def logistic(x,dist):
   x01 = __logistic(x)
-  x = uniform_rev(x01,dists)
+  x = uniform_rev(x01,dist)
   return x
 # Revert to original uniform distributions
-def uniform_rev(x01,dists):
-  x = np.zeros_like(x01)
-  for i in range(len(dists)):
-    intv = dists[i].interval(1.0)
-    x[:,i] = x01[:,i]*(intv[1]-intv[0])+intv[0]
+def uniform_rev(x,dist):
+  intv = dist.interval(1.0)
+  x = x*(intv[1]-intv[0])+intv[0]
   return x
 # Revert to original uniform distributions
-def normal_rev(xs,dists):
-  x = np.zeros_like(xs)
-  for i in range(len(dists)):
-    x[:,i] = xs[:,i]*dists[i].std()+dists[i].mean()
+def normal_rev(x,dist):
+  x = x*dist.std()+dist.mean()
   return x
 # Revert logarithm with power
 def log_rev(y):
@@ -93,17 +85,17 @@ def nonneg_rev(y):
 # Define class wrappers for matching sets of conversions and reversions
 # Also allows a standard format for use in surrogates without worrying about function arguments
 class normal:
-  def __init__(self,dists):
-    self.con = partial(std_normal,dists=dists)
-    self.rev = partial(normal_rev,dists=dists)
+  def __init__(self,dist):
+    self.con = partial(std_normal,dist=dist)
+    self.rev = partial(normal_rev,dist=dist)
 class uniform:
-  def __init__(self,dists):
-    self.con = partial(std_uniform,dists=dists)
-    self.rev = partial(uniform_rev,dists=dists)
+  def __init__(self,dist):
+    self.con = partial(std_uniform,dist=dist)
+    self.rev = partial(uniform_rev,dist=dist)
 class logit_logistic:
-  def __init__(self,dists):
-    self.con = partial(logit,dists=dists)
-    self.rev = partial(logistic,dists=dists)
+  def __init__(self,dist):
+    self.con = partial(logit,dist=dist)
+    self.rev = partial(logistic,dist=dist)
 class nonneg:
   def __init__(self):
     self.con = nonneg_con
