a85596d862336a230963d711f59be2b2b786ee4d
Andrew Angus <andrew.angus@warwick.ac.uk>
2021-06-04 18:11:09 +0100

    Remove redundant gpmap class

--- andvaranaut/inverse.py
+++ andvaranaut/inverse.py
@@ -241,83 +241,12 @@ class GPMAP(MAP,GP):
     print(f'Posterior is: {-res.fun:0.3f}')
     print(f'Time taken: {t1-t0:0.1f} s')
 
-# Quick and dirty maximum a posteriori class using a GP
-class gpmap:
-  def __init__(self,nx_exp,nx_model,gp):
-    # Check inputs
-    if (not isinstance(nx_exp,int)) or (nx_exp < 0):
-      raise Exception('Error: must specify an integer number of model input parameters => 0') 
-    if (not isinstance(nx_model,int)) or (nx_model < 1):
-      raise Exception('Error: must specify an integer number of model input parameters > 0') 
-    if (not isinstance(gp,GP)):
-      raise Exception("Error: must provide gp class instance from andvaranaut.forward module")
-    if (nx_exp+nx_model != gp.nx):
-      raise Exception("Error: nx_exp and nx_model must sum to gp.nx")
-
-    # Initialise attributes
-    self.nx_exp = nx_exp # Number of experimental inputs
-    self.nx_model = nx_model # Number of model inputs
-    self.gp = gp
-    self.xopt = None
-    self.xcopt = None
-    self.yexp = None
-    self.ycexp = None
-
-  def set_y(self,y):
-    self.xext = np.r_[self.gp.x,np.zeros((len(y),self.gp.nx))]
-    self.yext = np.r_[self.gp.y,y]
-    self.yexp = y
-    for i in range(self.gp.nx):
-      self.xext[:,i] = self.gp.xconrevs[i].con(self.xext[:,i])
-    for i in range(self.gp.ny):
-      self.yext[:,i] = self.gp.yconrevs[i].con(self.yext[:,i])
-    self.ycexp = self.yext[-len(y):]
-
-  def log_prior(self,x):
-    logps = np.zeros(self.gp.nx)
-    for i in range(self.gp.nx):
-      logps[i] = self.gp.xconrevs[i].prior.logpdf(x[i])
-    return np.sum(logps)
-
-  def log_likelihood(self,x):
-    gp2 = copy.deepcopy(self.gp)
-    gp2.xc = self.xext
-    gp2.xc[-1] = x
-    gp2.yc = self.yext
-    gp2.m = gp2._gp__fit(gp2.xc,gp2.yc,opt=False)
-    gp2.m.kern.lengthscale = self.gp.m.kern.lengthscale
-    gp2.m.kern.variance = self.gp.m.kern.variance
-    gp2.m.Gaussian_noise.variance = self.gp.m.Gaussian_noise.variance
-    return gp2.m.log_likelihood()
-
-  def log_posterior(self,x):
-    return self.log_likelihood(x) + self.log_prior(x)
-
-  def __negative_log_posterior(self,x):
-    return -self.log_posterior(x)
-    
-  def MAP(self):
-    # Bounds which try and avoid extrapolation
-    bnds = tuple((np.min(self.gp.xc[:,j]),np.max(self.gp.xc[:,j])) for j in range(self.gp.nx))
-    print("Finding optimal inputs by maximising posterior. Bounds on xc are:")
-    print(bnds)
-    res = differential_evolution(self.__negative_log_posterior,bounds=bnds)
-    resx = np.zeros(self.gp.nx)
-    for i in range(self.gp.nx):
-      resx[i] = self.gp.xconrevs[i].rev(res.x[i])
-    self.xopt = resx
-    self.xcopt = res.x
-
-    print(f'Optimal converted model parameters are: {res.x}')
-    print(f'Reverted optimal model parameters are: {resx}')
-    print(f'Posterior is: {-res.fun:0.3f}')
-
 # MCMC class inheriting from MAP
-class mcmc(MAP):
+class MCMC(MAP):
   def __init__(self,**kwargs):
     super().__init__(**kwargs)
 
 # GP-MCMC class inheriting from mcmc
-class gpmcmc(mcmc):
+class GPMCMC(GPMAP,MCMC):
   def __init__(self,gp,nx_exp,nx_model,**kwargs):
     super().__init__(**kwargs)
