296ad005ec61fedc441166752c220f9b085cde92
Andrew Angus <andrew.angus@warwick.ac.uk>
2022-09-14 16:04:52 +0100

    fix non-opt option on test plots

--- andvaranaut/forward.py
+++ andvaranaut/forward.py
@@ -538,6 +538,8 @@ class GP(_surrogate):
     mtrain = self.__fit(self.xtrain,self.ytrain,restarts=restarts,opt=opt,normalise=False)
     if not opt and self.m is not None:
       mtrain.kern.lengthscale = self.m.kern.lengthscale
+      mtrain.kern.variance = self.m.kern.variance
+      mtrain.Gaussian_noise.variance = self.m.Gaussian_noise.variance
     #ypred,ypred_var = mtrain.predict(self.xtest)
     ypred = self.__predict(mtrain,self.xtest,return_var=False)
     # Either revert data to original for comparison or leave as is
