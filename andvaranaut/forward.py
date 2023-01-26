Affine derivative and likelihood minor fixes

andvaranaut/forward.py
@@ -703,8 +703,11 @@ class GP(_surrogate):
 
     # Warping term
     warp = 0
-    for i in range(self.ny):
-      warp += np.sum(np.log(self.yconrevs[i].der(self.y)))
+    try:
+      for i in range(self.ny):
+        warp += np.sum(np.log(self.yconrevs[i].der(self.y)))
+    except:
+      pass
 
     return baseLL + warp
 

andvaranaut/utils.py
@@ -253,7 +253,7 @@ class affine:
   def rev(self,y):
     return (y-self.a)/self.b
   def der(self,y):
-    return self.b
+    return self.b*np.power(y,0)
 class meanstd(affine):
   def __init__(self,y):
     mean = np.mean(y)